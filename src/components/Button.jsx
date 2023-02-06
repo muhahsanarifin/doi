f06042ad92bd072f4c24b5c2bd408ec4a3b8c0ae
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-02-06 22:37:41 +0700

    Add Buttons

---
+++ src/components/Button.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import styles from "../styles/Button.module.css";
+
+const LoginButton = ({ password, email }) => {
+  return (
+    <>
+      <button
+        className={styles[!password || !email ? "btn" : "btn-active"]}
+        disabled={!password || !email}
+      >
+        Login
+      </button>
+    </>
+  );
+};
+
+const SignUpButton = ({ firstName, lastName, email, password }) => {
+  return (
+    <>
+      <button
+        className={
+          styles[
+            !firstName || !lastName || !password || !email
+              ? "btn"
+              : "btn-active"
+          ]
+        }
+      >
+        {" "}
+        SignUp{" "}
+      </button>
+    </>
+  );
+};
+
+const ForgotPasswordButton = ({ email }) => {
+  return (
+    <>
+      <button
+        className={styles[!email ? "btn" : "btn-active"]}
+        disabled={!email}
+      >
+        Confirm
+      </button>
+    </>
+  );
+};
+
+export { LoginButton, SignUpButton, ForgotPasswordButton };
