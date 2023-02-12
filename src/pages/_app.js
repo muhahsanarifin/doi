7e0086b5531ea208e3fcbc6f202cd77e049d23b2
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-02-12 11:27:29 +0700

    Updated _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import store, { persistedStore } from "../redux/store";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Provider store={store}>
+        <PersistGate persistor={persistedStore} loading={null}>
+          <Component {...pageProps} />
+        </PersistGate>
+      </Provider>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
