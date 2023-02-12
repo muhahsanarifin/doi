0f5108e356aced7e0bb0e3356938c24be743caaa
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-02-12 11:28:08 +0700

    Add store.js

---
+++ src/redux/store.js
@@ -0,0 +1,23 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import reducers from "./reducers/index";
+import logger from "redux-logger";
+
+const persitConfig = {
+  key: "doi",
+  storage,
+};
+
+const persistedReducer = persistReducer(persitConfig, reducers);
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ thunk: true, serializableCheck: false }).concat(
+      logger
+    ),
+});
+
+export const persistedStore = persistStore(store);
+
+export default store;
