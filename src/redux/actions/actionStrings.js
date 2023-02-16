3e3ee8b335b0f2a8e9eec7c1e9aa26c416525958
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-02-16 11:00:11 +0700

    Updated actionString.js

--- src/redux/actions/actionStrings.js
+++ src/redux/actions/actionStrings.js
@@ -1,4 +1,11 @@
 export const actionStrings = {
   // Dashboard
   getDataDashboard: "DATA_DASHBOARD_GET",
+
+  // History Transaction
+  getHistoryTransaction: "HISTORY_TRANSACTION_GET",
+
+  // Users
+  getDataUser: "DATA_USER_GET",
+  getDataReceiver: "DATA_RECEIVER_GET",
 };
