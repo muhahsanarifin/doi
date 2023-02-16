4aa0a2a47cc0f13a0938d34adb06766134636484
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-02-16 10:59:47 +0700

    Updated index.js

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import dashboardReducer from "./dashboard";
+import transactionHistoryReducer from "./history";
+import usersReducer from "./user";
 
 export default combineReducers({
   dashboard: dashboardReducer,
+  historyTransaction: transactionHistoryReducer,
+  users: usersReducer,
 });
