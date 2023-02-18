928fdc4547488a33be32fda444b2b8e87f6b97d4
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-02-18 13:47:26 +0700

    Updated reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,16 @@
 import { combineReducers } from "@reduxjs/toolkit";
+import authReducer from "./auth";
+import usersReducer from "./user";
 import dashboardReducer from "./dashboard";
 import transactionHistoryReducer from "./history";
-import usersReducer from "./user";
+import topUpReducer from "./topup";
+import transferReducer from "./transfer";
 
 export default combineReducers({
+  auth: authReducer,
   dashboard: dashboardReducer,
   historyTransaction: transactionHistoryReducer,
   users: usersReducer,
+  topUp: topUpReducer,
+  transfer: transferReducer,
 });
--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -2,20 +2,15 @@ import { ActionType } from "redux-promise-middleware";
 import { actionStrings } from "../actions/actionStrings";
 
 const intialState = {
+  getDataUsers: [],
   getDataUser: {},
-  getDataReceiver: {
-    status: null,
-    msg: null,
-    data: {
-      id: null,
-      firstName: null,
-      lastName: null,
-      email: null,
-      image: null,
-      noTlp: null,
-      balance: null,
-    },
-  },
+  getDataReceiver: {},
+  checkPinUser: {},
+  updateProfileUser: {},
+  updateImageUser: {},
+  updatePinUser: {},
+  updatePasswordUser: {},
+  deleteImageUser: {},
   isLoading: false,
   isError: false,
   isFulfilled: false,
@@ -24,8 +19,45 @@ const intialState = {
 
 const usersReducer = (prevState = intialState, { payload, type }) => {
   const { Pending, Fulfilled, Rejected } = ActionType;
-  const { getDataUser, getDataReceiver } = actionStrings;
+  const {
+    getDataUsers,
+    getDataUser,
+    getDataReceiver,
+    checkPinUser,
+    updateProfileUser,
+    updateImageUser,
+    updatePinUser,
+    updatePasswordUser,
+    deleteImageUser,
+  } = actionStrings;
   switch (type) {
+    case getDataUsers.concat("-", Pending):
+      return {
+        ...prevState,
+        err: null,
+        isLoading: true,
+        isFulfilled: false,
+        isError: false,
+      };
+    case getDataUsers.concat("-", Fulfilled):
+      return {
+        ...prevState,
+        isLoading: false,
+        isFulfilled: true,
+        getDataUsers: payload.data,
+        isError: false,
+        err: null,
+      };
+
+    case getDataUsers.concat("-", Rejected): {
+      return {
+        ...prevState,
+        isLoading: false,
+        isFulfilled: false,
+        isError: true,
+        err: payload.error.message, // <= Default error message
+      };
+    }
     case getDataUser.concat("-", Pending):
       return {
         ...prevState,
@@ -78,6 +110,174 @@ const usersReducer = (prevState = intialState, { payload, type }) => {
         isFulfilled: false,
         isError: true,
         err: payload.error.message, // <= Default error message
+      };
+    }
+
+    case checkPinUser.concat("-", Pending):
+      return {
+        ...prevState,
+        err: null,
+        isLoading: true,
+        isFulfilled: false,
+        isError: false,
+      };
+    case checkPinUser.concat("-", Fulfilled):
+      return {
+        ...prevState,
+        isLoading: false,
+        isFulfilled: true,
+        checkPinUser: payload.data,
+        isError: false,
+        err: null,
+      };
+
+    case checkPinUser.concat("-", Rejected): {
+      return {
+        ...prevState,
+        isLoading: false,
+        isFulfilled: false,
+        isError: true,
+        err: payload.error.response.data?.msg, // <= Custome error message
+      };
+    }
+
+    case updateProfileUser.concat("-", Pending):
+      return {
+        ...prevState,
+        err: null,
+        isLoading: true,
+        isFulfilled: false,
+        isError: false,
+      };
+    case updateProfileUser.concat("-", Fulfilled):
+      return {
+        ...prevState,
+        isLoading: false,
+        isFulfilled: true,
+        updateProfileUser: payload.data,
+        isError: false,
+        err: null,
+      };
+
+    case updateProfileUser.concat("-", Rejected): {
+      return {
+        ...prevState,
+        isLoading: false,
+        isFulfilled: false,
+        isError: true,
+        err: payload.error.response.data?.msg, // <= Custome error message
+      };
+    }
+
+    case updateImageUser.concat("-", Pending):
+      return {
+        ...prevState,
+        err: null,
+        isLoading: true,
+        isFulfilled: false,
+        isError: false,
+      };
+    case updateImageUser.concat("-", Fulfilled):
+      return {
+        ...prevState,
+        isLoading: false,
+        isFulfilled: true,
+        updateImageUser: payload.data,
+        isError: false,
+        err: null,
+      };
+
+    case updateImageUser.concat("-", Rejected): {
+      return {
+        ...prevState,
+        isLoading: false,
+        isFulfilled: false,
+        isError: true,
+        err: payload.error.response.data?.msg, // <= Custome error message
+      };
+    }
+
+    case updatePinUser.concat("-", Pending):
+      return {
+        ...prevState,
+        err: null,
+        isLoading: true,
+        isFulfilled: false,
+        isError: false,
+      };
+    case updatePinUser.concat("-", Fulfilled):
+      return {
+        ...prevState,
+        isLoading: false,
+        isFulfilled: true,
+        updatePinUser: payload.data,
+        isError: false,
+        err: null,
+      };
+
+    case updatePinUser.concat("-", Rejected): {
+      return {
+        ...prevState,
+        isLoading: false,
+        isFulfilled: false,
+        isError: true,
+        err: payload.error.response.data?.msg, // <= Custome error message
+      };
+    }
+
+    case updatePasswordUser.concat("-", Pending):
+      return {
+        ...prevState,
+        err: null,
+        isLoading: true,
+        isFulfilled: false,
+        isError: false,
+      };
+    case updatePasswordUser.concat("-", Fulfilled):
+      return {
+        ...prevState,
+        isLoading: false,
+        isFulfilled: true,
+        updatePasswordUser: payload.data,
+        isError: false,
+        err: null,
+      };
+
+    case updatePasswordUser.concat("-", Rejected): {
+      return {
+        ...prevState,
+        isLoading: false,
+        isFulfilled: false,
+        isError: true,
+        err: payload.error.response.data?.msg, // <= Custome error message
+      };
+    }
+
+    case deleteImageUser.concat("-", Pending):
+      return {
+        ...prevState,
+        err: null,
+        isLoading: true,
+        isFulfilled: false,
+        isError: false,
+      };
+    case deleteImageUser.concat("-", Fulfilled):
+      return {
+        ...prevState,
+        isLoading: false,
+        isFulfilled: true,
+        deleteImageUser: payload.data,
+        isError: false,
+        err: null,
+      };
+
+    case deleteImageUser.concat("-", Rejected): {
+      return {
+        ...prevState,
+        isLoading: false,
+        isFulfilled: false,
+        isError: true,
+        err: payload.error.response.data?.msg, // <= Custome error message
       };
     }
 
