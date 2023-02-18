99edcc1fcb551bcc4fe66f2492b0b75183d6bc92
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-02-18 13:46:36 +0700

    Updated actionStrings.js

--- src/redux/actions/actionStrings.js
+++ src/redux/actions/actionStrings.js
@@ -6,6 +6,27 @@ export const actionStrings = {
   getHistoryTransaction: "HISTORY_TRANSACTION_GET",
 
   // Users
+  getDataUsers: "DATA_USERS_GET",
   getDataUser: "DATA_USER_GET",
   getDataReceiver: "DATA_RECEIVER_GET",
+  checkPinUser: "PIN_USER_CHECK",
+  updateProfileUser: "PROFILE_UPDATE_USER",
+  updateImageUser: "IMAGE_USER_UPDATE",
+  updatePinUser: "PIN_USER_UPDATE",
+  updatePasswordUser: "USER_PASSWORD_UPDATE",
+  deleteImageUser: "IMAGE_USER_DELETE",
+
+  // Auth
+  register: "REGISTER",
+  login: "LOGIN",
+  logout: "LOGOUT",
+  forgotPassword: "FORGOT_PASSWORD",
+  resetPassword: "RESET_PASSWORD",
+  verify: "VERIFY",
+
+  // Top Up
+  topUp: "TOPUP",
+
+  // Transfer
+  transfer: "TRANSFER",
 };
