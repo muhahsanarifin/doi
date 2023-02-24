d425e466864bd05b2a1b82a4ac4da27a92be4c9c
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-02-24 13:50:34 +0700

    Updated store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import reducers from "./reducers/index";
-import logger from "redux-logger";
+// import logger from "redux-logger"; // <- Disable log
 
 const persitConfig = {
   key: "doi",
@@ -13,9 +13,10 @@ const persistedReducer = persistReducer(persitConfig, reducers);
 const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({ thunk: true, serializableCheck: false }).concat(
-      logger
-    ),
+    getDefaultMiddleware({ thunk: true, serializableCheck: false }),
+  // .concat(
+  //   logger
+  // ),
 });
 
 export const persistedStore = persistStore(store);
