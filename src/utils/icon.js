0a893f0c7612181ac5d5724e31c289472695a3a9
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-09-20 14:02:16 +0700

    Add, Update Package + assets + utils

---
+++ src/utils/icon.js
@@ -0,0 +1,75 @@
+import mail from "../assets/icons/mail.png";
+import mailBlue from "../assets/icons/mail-blue.png";
+import mailRed from "../assets/icons/mail-red.png";
+import lock from "../assets/icons/lock.png";
+import lockBlue from "../assets/icons/lock-blue.png";
+import lockRed from "../assets/icons/lock-red.png";
+import person from "../assets/icons/person.png";
+import personBlue from "../assets/icons/person-blue.png";
+import success from "../assets/icons/success.png";
+import failed from "../assets/icons/failed.png";
+import arrowUpBlue from "../assets/icons/arrow-up-blue.png";
+import arrowUpBlueMagenta from "../assets/icons/arrow-up-blue-magenta.png";
+import plusBlueMagenta from "../assets/icons/plus-blue-magenta.png";
+import plusBlue from "../assets/icons/plus-blue.png"
+import gridBlue from "../assets/icons/grid-blue.png";
+import grid from "../assets/icons/grid.png";
+import arrowUp from "../assets/icons/arrow-up.png";
+import plus from "../assets/icons/plus.png";
+import user from "../assets/icons/user.png";
+import userBlue from "../assets/icons/user-blue.png";
+import logOut from "../assets/icons/log-out.png";
+import arrowUpGreen from "../assets/icons/arrow-up-green.png";
+import arrowUpRed from "../assets/icons/arrow-up-red.png";
+import arrowDownGreen from "../assets/icons/arrow-down-green.png";
+import arrowDownRed from "../assets/icons/arrow-down-red.png";
+import arrowLeft from "../assets/icons/arrow-left.png";
+import phone from "../assets/images/png-phone.png";
+import phoneSecond from "../assets/images/png-phone-2.png";
+import pen from "../assets/icons/edit-2.png";
+import penBlue from "../assets/icons/edit-2-blue.png";
+import telp from "../assets/icons/phone.png";
+import telpBlue from "../assets/icons/phone-blue.png";
+import telpRed from "../assets/icons/phone-red.png";
+import edit from "../assets/icons/edit-2.png";
+import bell from "../assets/icons/bell.png";
+
+const icon = {
+  mail,
+  mailBlue,
+  mailRed,
+  lock,
+  lockBlue,
+  lockRed,
+  person,
+  personBlue,
+  success,
+  failed,
+  arrowUpBlue,
+  arrowUpBlueMagenta,
+  plusBlueMagenta,
+  plusBlue,
+  gridBlue,
+  grid,
+  arrowUp,
+  plus,
+  user,
+  userBlue,
+  logOut,
+  arrowUpGreen,
+  arrowUpRed,
+  arrowDownGreen,
+  arrowDownRed,
+  arrowLeft,
+  phone,
+  phoneSecond,
+  pen,
+  penBlue,
+  telp,
+  telpBlue,
+  telpRed,
+  edit,
+  bell,
+};
+
+export default icon;
